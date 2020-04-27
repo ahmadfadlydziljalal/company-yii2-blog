513cb32f66e26db990bca03a30c4c4cbb1d949eb
dziljalal1 <dziljalal1@gmail.com>
2020-04-27 15:33:31 +0700

    Merubah koneksi app dengan db container menggunakan environment pada docker-compose

--- php/yii2/config/db.php
+++ php/yii2/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=${DB_HOST};dbname=${DB_NAME}',
+    'username' => '${DB_ROOT_USER}',
+    'password' => '${DB_ROOT_PASSWORD}',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
