21e2e219b0423fffbb2069f2155fb879864fef7e
dziljalal1 <dziljalal1@gmail.com>
2020-04-29 17:29:48 +0700

    Image version ditetapkan pada dependensi docker image yang digunakan

--- yii2/views/site/index.php
+++ yii2/views/site/index.php
@@ -4,56 +4,47 @@
 
 use yii\helpers\VarDumper;
 
-$this->title = 'My Yii Application';
+$this->title = getenv("APP_NAME");
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1><?= getenv('DB_DSN') ?></h1>
-
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h1><?= Yii::powered()  ?></h1>
+        <p class="lead">Versi: <?= Yii::getVersion()  ?></p>
     </div>
 
     <div class="body-content">
-
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>MySQL</h2>
+                <p>FROM mysql:5.7</p>
+                <hr>
+                <p>
+                    MySQL Database Service is a fully managed database service to deploy
+                    cloud-native applications using the world’s most popular open source database.
+                    It is 100% developed, managed and supported by the MySQL Team.
+                </p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-
-        </pre>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" target="_blank" href="https://www.mysql.com/">MySQL Docs &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Nginx</h2>
+                <p>FROM nginx:1.18.0-alpine</p>
+                <hr>
+                <p>
+                    NGINX accelerates content and application delivery, improves security,
+                    facilitates availability and scalability for the busiest web sites on the Internet.
+                </p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-default" target="_blank" href="https://www.nginx.com/">NGINX Docs &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Adminer</h2>
+                <p>FROM adminer:4.7.6</p>
+                <hr>
+                <p>Adminer (formerly phpMinAdmin) is a full-featured database management tool written in PHP. </p>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://www.adminer.org/">Adminer Docs &raquo;</a></p>
             </div>
         </div>
 
